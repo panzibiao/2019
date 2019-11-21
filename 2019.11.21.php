19eec8de288b630cc30653db978393447e38bd53
panzibiao <56524645+panzibiao@users.noreply.github.com>
2019-11-21 16:39:50 +0800

    Create 2019.11.21.php

---
+++ 2019.11.21.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/reset.css"/>
+	<style type="text/css">
+		body{
+			background:url(./images/bd-bg.png) repeat-x;
+		}
+		.nav{
+			float: right;
+			margin-top:15px;
+		}
+		.nav li{
+			float: left;
+			padding: 0 10px;
+			border-left: 1px #d6d6d6 dotted;
+		}
+		.nav a{
+			font:bold 14px Georgia;
+			color: #666;
+			text-decoration: none;
+		}
+		.nav p{
+			color: #b7b7b7;
+			font:11px tahoma;
+		}
+		.header{
+			width: 940px;
+			margin: 0 auto;
+			padding-top:37px;
+			padding-bottom: 46px;
+		}
+		.logo{
+			margin-left: 15px;
+		}
+		.nav a:hover{
+			color: #a1a1a1;
+		}
+		.banner{
+            width: 940px
+            margin: 0 auto ;
+		}
+		.pointerDiv{
+
+		}
+		.pointerDiv a{
+			display: block;
+			float: left;
+			margin-left: 4px
+			width: 17px
+			height: 17px;
+			background-image: url(./images/pointer.png);
+
+		}
+		.pointntDiv a
+		.pointntDiv a:hover;{
+			background: url(./images/pointer.png);
+		}
+		.content{
+			width: 940px;
+			margin: 0 auto;
+		}
+		.content h1{
+			text-align: center;
+			font: bold 24px Georgia;
+		}
+		.content h1{
+			text-align: center;
+			font: bold 24px Georgia;
+			padding: 6px 0 20px 0;
+			margin-bottom: 38px;
+			background: url() no-repeat bottom center;
+		}
+		.content .pl,.content .cs,.content .uc{
+			float: left;
+			width: 300px;
+		}
+		.content  .cs{
+			margin:  0 20px;
+		}
+		.content h2{
+			color: #11719;
+			font:21px Georgia;
+		}
+		.content .p1{
+			color: #949494;
+			font: 12px Helvetica;
+
+		}
+		.content .imgDiv{
+			width: 300px;
+			height: 190px;
+			margin: 16px 0px 10px 0px;
+			padding-top:12px;
+		}
+		.content .p2{
+			height: 92px
+			color:#3e3e3e;
+			font:13px Helvetica;
+			word-spacing: 2px;
+		}
+		.content .lm{
+			display: block;
+			width: 163px;
+			height: 40px;
+			text-decoration: none;
+			color: yellow;
+			font: 12px Helvetica;
+		    line-height: 40px;
+		    text-indent: 1em;
+		    margin-bottom: 35px;
+		    background: url(./img/btn1.png) no-repeat;
+		}
+	</style>
+</head>
+<body>
+	<div class="header">
+		<ul class="nav">
+		<li>
+			<a href="#">HOME</a>
+			<p>back to home</p>
+		</li>
+		<li>
+			<a href="#">HOME</a>
+			<p>back to home</p>
+		</li>
+		<li>
+			<a href="#">HOME</a>
+			<p>back to home</p>
+		</li>
+		<li>
+			<a href="#">HOME</a>
+			<p>back to home</p>
+		</li>
+		<li>
+			<a href="#">HOME</a>
+			<p>back to home</p>
+		</li>
+	</ul>
+	<div class="logo">
+		<a href="#" title="这是logo">
+			<img src="./img/logo.png" alt="网站logo" />
+		</a>
+	</div> 
+	</div>
+	<div class="banner">
+		<img src="./img/banner.png" alt="这是轮播图">
+		<div class="pointerDiv">
+			<a href="#"></a>
+			<a class="active" href="#"></a>
+			<a href="#"></a>
+			<a href="#"></a>
+			<a href="#"></a>
+          </div>
+      </div>
+    <div class="content">
+    	<h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h1>
+    	<div class="pl">
+    		<h2>perfect logic</h2>
+    		<p class="p1">All you want your website to do.</p>
+    		<div class="imgDIiv">
+    			<img src="./img/pic/pic1.jpg" alt="小男孩">
+    		</div>
+    		<p class="p2">
+    			Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+            </p>
+            <a class="lm" href="#">Learn more</a>
+        </div>
+        <div class="cs">
+    		<h2>perfect logic</h2>
+    		<p class="p1">All you want your website to do.</p>
+    		<div class="imgDIiv">
+    			<img src="./img/pic/pic2.jpg" alt="小男孩">
+    		</div>
+    		<p class="p2">
+    			Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+            </p>
+            <a class="lm" href="#">Learn more</a>
+        </div>
+        <div class="uc">
+    		<h2>perfect logic</h2>
+    		<p class="p1">All you want your website to do.</p>
+    		<div class="imgDIiv">
+    			<img src="./img/pic/pic3.jpg" alt="小男孩">
+    		</div>
+    		<p class="p2">
+    			Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+            </p>
+            <a class="lm" href="#">Learn more</a>
+        </div>
+    </div>
+
+ 
+</body>
+</html>
+
